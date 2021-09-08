5609de5fc0d379fb471278c72d1e8fd3819c2f8e
Ayoub El-Haddadi <ayoub1998elh@gmail.com>
2021-09-08 20:49:24 +0300

    add login and logout

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -2,21 +2,33 @@ import React from 'react';
 import '../css/header.css';
 import logo from '../images/logo.png';
 import Navigation from './NavigationComponent';
-import { Link } from "react-router-dom";
+import { Link, useHistory} from "react-router-dom";
 
 function Header() {
-        return <nav className="navbar">
-                <ul className="left-elements">
-                    <li><Navigation/></li>
-                    <li><img className="logo-icone" src={logo} alt="Logo"/> </li>
-                    <li className="left-elem"><Link to='/'>Home</Link></li>
-                    <li className="left-elem"><Link to="/contact-us">Contact us</Link></li>
-                </ul>
-                <ul className="right-elements">
-                    <li className="btn-li"><Link to="/auth/login">Log in</Link></li>
-                    <li className="btn-li"><Link to="/auth/register">Register</Link></li>
-                </ul>
-            </nav>
+    const history = useHistory()
+    function logout(){
+        localStorage.removeItem('user-info')
+        history.push('/auth/login')
+    }
+    let rightElement =  <ul className="right-elements">
+                            <li className="btn-li"><Link to="/auth/login">Log in</Link></li>
+                            <li className="btn-li"><Link to="/auth/register">Register</Link></li>
+                        </ul>;
+    if(localStorage.getItem('user-info')){
+        rightElement = <ul className="right-elements">
+                            <li className="btn-li"><Link>Profile</Link></li>
+                            <li className="btn-li" onClick={logout}>Logout</li>
+                        </ul>
+    }
+    return <nav className="navbar">
+            <ul className="left-elements">
+                <li><Navigation/></li>
+                <li><img className="logo-icone" src={logo} alt="Logo"/> </li>
+                <li className="left-elem"><Link to='/'>Home</Link></li>
+                <li className="left-elem"><Link to="/contact-us">Contact us</Link></li>
+            </ul>
+            {rightElement}
+        </nav>
 }
 
 export default Header
--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,32 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/login.css';
 import logo from '../images/logo.png';
-import { Link } from "react-router-dom";
-
+import { Link ,useHistory} from "react-router-dom";
+import axios from 'axios';
 function Login(){
+    const [login, setLogin] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [fail, setfail] = useState(null)
+    const history = useHistory()
+    useEffect(()=>{
+            if(localStorage.getItem("user-info")){
+                history.push('/');
+            }
+    }, [])
+    async function Submit(){
+        let info = {login, email, password};
+        const loginUrl = `http://127.0.0.1:8000/api/auth/login`;
+        
+        let result  = await fetch(loginUrl, {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json',
+                "Accept": 'application/json'
+            },
+            body: JSON.stringify(info)
+        })
+        result = await result.json()
+        let resultArr = JSON.parse(JSON.stringify(result));
+        if(resultArr['error'] || resultArr['message']){
+            setfail(resultArr['error'] || resultArr['message'])
+        }else{
+            setfail(null)
+            localStorage.setItem("user-info", JSON.stringify(result));
+            history.push('/');
+        }
+    }
     return <div>
                 <div class="login_container"> 
                     <img className="logo-icon-login" src={logo} alt="Logo"/>
                     <div class="login_container__card">
-                        <form>
+                    {fail ? ( <div className="error"><p>{fail}</p></div>
+                            ) : (fail)
+                            }
                             <div className="inputField">
                                 <label for="inputLogin">Login</label>
-                                <input id="inputLogin" type="text" name="login" placeholder="Input your username..."/>
+                                <input id="inputLogin" type="text" name="login" onChange={(e) => setLogin(e.target.value)} placeholder="Input your username..."/>
                             </div>
                             <div className="inputField">
                                 <label for="inputEmail">Email</label>
-                                <input id="inputEmail" type="email" name="email" placeholder="Input your email..."/>
+                                <input id="inputEmail" type="email" name="email" onChange={(e) => setEmail(e.target.value)} placeholder="Input your email..."/>
                             </div>
                             <div className="inputField">
                                 <label for="inputPassword">Password</label>
-                                <input id="inputPassword" type="password" name="password" placeholder="Input your password..."/>
+                                <input id="inputPassword" type="password" name="password" onChange={(e) => setPassword(e.target.value)} placeholder="Input your password..."/>
                             </div>
-                            <button className="login_btn" type="submit">Log in</button>
+                            <button className="login_btn" onClick={Submit} type="submit">Log in</button>
                             <div className="inputField">
                                 <Link to="/auth/register" className="login_link">Not registed yet?</Link>
                                 <Link to="/auth/password-reset" className="login_link">Fogot password ?</Link>
                             </div>
-                        </form>
                     </div>
                 </div>
            </div>
